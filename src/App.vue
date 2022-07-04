0a87bc4b47da6b0379f1be2298a66ea049088c94
Damien Caselli <damien.caselli@vibe.com>
2022-07-04 09:43:46 +0200

    feat: add an ugly yet functional button-ish component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
 </script>
 
 <style>
---
+++ src/components/BaseAction.vue
@@ -0,0 +1,26 @@
+<template>
+  <component
+    :is="$attrs.href ? 'a' : 'button'"
+    v-bind="$attrs"
+    class="base-button"
+    ><slot>Default text</slot></component
+  >
+</template>
+
+<style scoped>
+.base-button {
+  display: inline-block;
+  padding: 0.25rem 0.5rem;
+
+  color: #333;
+  cursor: pointer;
+  border: 2px solid #333;
+  background-color: #efefe5;
+  border-radius: 4px;
+
+  text-decoration: none;
+  font-size: 100%;
+  font-family: inherit;
+  line-height: 1;
+}
+</style>
